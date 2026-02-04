291c086add49178b791b5853862946f3a2d5adff
Hungnth <thienhungnth@gmail.com>
2026-02-04 17:44:51 +0700

    refactor: remove unused FilamentShield facade from AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers\Filament;
 
-use BezhanSalleh\FilamentShield\Facades\FilamentShield;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
