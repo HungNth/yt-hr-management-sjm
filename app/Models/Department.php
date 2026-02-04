f92d16bdf8341bea8f91229890eae5cc1ed7fb9b
Hungnth <thienhungnth@gmail.com>
2026-02-04 14:14:47 +0700

    add relationships to Department model (manager, employees, positions)

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -3,6 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Department extends Model {
+class Department extends Model
+{
+
+    protected $fillable = ['name', 'description', 'color', 'manager_id'];
+
+    public function manager(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'manager_id');
+    }
+
+    public function employees(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function positions(): HasMany
+    {
+        return $this->hasMany(Position::class);
+    }
 }
