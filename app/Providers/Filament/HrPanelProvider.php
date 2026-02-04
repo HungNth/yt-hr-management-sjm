4acbf85f9f8499fe4b88ec9f15f78ab22631add8
Hungnth <thienhungnth@gmail.com>
2026-02-04 17:44:44 +0700

    feat: integrate FilamentShield plugin into HrPanelProvider

--- app/Providers/Filament/HrPanelProvider.php
+++ app/Providers/Filament/HrPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -55,6 +56,8 @@ class HrPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])->plugins([
+                FilamentShieldPlugin::make(),
             ]);
     }
 }
